6bcc40b77ed99b77f4a6c6219de82522f13aeee5
truglygrl <truglygrl@gmail.com>
2024-03-18 16:16:21 +0300

    sss111

--- dz_46/p2.py
+++
@@ -1,8 +0,0 @@
-for x in range(800001, 900000):
-    s = 0
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            s = i + x // i
-            break
-    if s % 138 == 0 and s > 0:
-        print(x, s)

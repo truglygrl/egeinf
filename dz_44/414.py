85d67ec827c5a6d3f372e3641fb3a3ecfdaa62a4
truglygrl <truglygrl@gmail.com>
2024-03-26 12:11:20 +0300

    to smth

--- dz_44/414.py
+++ dz_44/414.py
@@ -1,4 +1,6 @@
-from ipaddress import *
+"""from ipaddress import *
 for mask in range(33):
     net = ip_network(f'186.153.196.175/{mask}', 0)
-    print(net, net.netmask)
+    print(net, net.netmask)"""
+print(*[x for x in range(18)], sep=', ')
+
